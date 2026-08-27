004246f30a45454f3046662269392ddf4a43b427
Pavel Zaytsev <pavel@iberisoft.com>
2026-08-27 23:57:46 +0200

    Update AppSettings and add YOLO configuration

--- LockPilot/AppSettings.cs
+++ LockPilot/AppSettings.cs
@@ -16,11 +16,13 @@ class AppSettings
 
     public required int[] DetectionColorBgr { get; init; }
 
-    public required double OrbIntervalSeconds { get; init; }
+    public required double RelocalizeIntervalSeconds { get; init; }
 
     public required int MinLkPoints { get; init; }
 
     public required double MaxLkError { get; init; }
+
+    public required YoloSettings Yolo { get; init; }
 
     public static AppSettings Load(string path) => JsonSerializer.Deserialize<AppSettings>(File.ReadAllText(path));
 
@@ -29,5 +31,14 @@ class AppSettings
         public required int Width { get; init; }
 
         public required int Height { get; init; }
+    }
+
+    public class YoloSettings
+    {
+        public required string ModelName { get; init; }
+
+        public required float Confidence { get; init; }
+
+        public required float IoU { get; init; }
     }
 }
