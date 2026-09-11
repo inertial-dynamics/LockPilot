84384d38241404432b5c11682b696ebaeb541d5a
Pavel Zaytsev <pavel@iberisoft.com>
2026-09-11 11:12:38 +0200

    Add CommandServer class with NetMQ PullSocket support

---
+++ LockPilot/CommandServer.cs
@@ -0,0 +1,28 @@
+using LockPilot.Shared;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace LockPilot;
+
+class CommandServer : IDisposable
+{
+    readonly PullSocket m_Socket = new();
+
+    public CommandServer(int port)
+    {
+        m_Socket.Options.Linger = TimeSpan.Zero;
+        m_Socket.Bind($"tcp://*:{port}");
+    }
+
+    public bool TryDequeue(out Command? command)
+    {
+        command = m_Socket.TryReceiveFrameString(out var json) ? CommandMessage.Deserialize(json) : null;
+        return command != null;
+    }
+
+    public void Dispose()
+    {
+        m_Socket.Dispose();
+        NetMQConfig.Cleanup();
+    }
+}
